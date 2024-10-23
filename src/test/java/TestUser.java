476e7eca8de09455a916698fd40b4f390b2bd491
erica-qa <erica.santos.qa@gmail.com>
2024-10-22 22:36:14 -0300

    first commit

---
+++ src/test/java/TestUser.java
@@ -0,0 +1,34 @@
+// 0 - Nome do pacote
+
+// 1 - Bibliotecas
+import java.nio.file.Files;
+
+import org.junit.jupiter.api.Test;
+
+// 2 - Classe
+public class TestUser {
+    // 2.1 Atributos
+
+    static String ct = "application/json"; // Content type
+    static String uriPet = "https://petstore.swagger.io/v2/pet"; // Base URL + endpoint
+
+    // 2.2 Funções e Métodos
+    // 2.2.1 Funções e Métodos Comuns / Úteis
+
+    // Função de Leitura de json
+    public static String lerArquivoJson(String arquivoJson){
+        //TODO: Completar a leitura do arquivo
+        //return new String(Files)
+        return null;
+    }
+
+
+
+    @Test
+    public void testPostPet(){
+// Carregar os dados do arquivo json do pet
+// TODO: Criar o método Post
+//String jsonBody = 
+    }
+
+}
